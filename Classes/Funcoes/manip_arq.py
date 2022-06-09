682a74bab85ddd7486fe21ea40f0f70365700553
Isau Junior <isaujr.hiago@gmail.com>
2022-06-08 23:17:47 -0300

    adiciona pacote com funções para manipular arquivos e acrecenta os metodos para carregar e salvar os dados da classe disciplina

---
+++ Classes/Funcoes/manip_arq.py
@@ -0,0 +1,46 @@
+import os
+
+def Buscar_arquivo(path, nome):
+    """
+    Verifica se o arquivo existe no caminho informado.
+
+    param: path -> Caminho onde se encontra o arquivo (tipo: str)
+    param: nome -> Nome do arquivo 
+    return: retorna True se o arquivo existir e False caso não exista
+    """
+    try:
+        with open(path + '/' + nome + '.txt'):
+            pass
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+def Excluir_arquivo(path, nome):
+    """
+    Exclui o arquivo .txt cujo nome e caminho foram informados 
+
+    param: path -> Camonho onde se encontra o arquivo (tipo: str)
+    param: nome -> Nome do arquivo (tipo: str)
+    """
+    if Buscar_arquivo(path, nome):
+        os.remove(path + '/' + nome + '.txt')
+    else:
+        print('Arquivo não encontrado')
+
+def Carregar_arquivo(path, nome, modo):
+    """
+    Abre um arquivo .txt com o nome indicado e salvo no caminho indicado e retorna essesdados para ser
+    carregado em uma variavel
+
+    param: path -> Caminho do arquivo (tipo: str)
+    param: nome -> Nome do arquivo sem extenção (tipo: str)
+    param: modo -> Modo no qual se deseja abrir o arquivo (tipo: str)
+    return: retorna uma string como os dados contidos no arquivo
+    """
+    with open(path+ '/' + nome +'.txt', modo) as arq:
+            return arq.read()
+
+#Excluir_arquivo('Dados_salvos/Disciplinas','DCO5362')
+#dado = Carregar_arquivo('Dados_salvos', str(202232648), 'r+')
+#print(dado)
--- Classes/classdisciplina.py
+++ Classes/classdisciplina.py
@@ -2,6 +2,9 @@
 Implementação da classe Disciplina. A classe disciplina é a classe que quada os dados das displinas ministradas
 pelos professores e cursada pelos alunos. no projeto MiniSIGAA
 """
+from Funcoes.manip_arq import Carregar_arquivo
+
+pathDisciplinas = 'Dados_salvos/Disciplinas'
 
 class Disciplina:
     def __init__(self, ID='', Nome='',  Professor = '', Alunos = ''):
@@ -10,7 +13,8 @@ class Disciplina:
         self.__Professor = Professor
         self.__Alunos = Alunos
 
-    def Set_aluno(self, Alunos):
+    #metodos para receber os atribudos do objeto 
+    def Set_alunos(self, Alunos):
         self.__Alunos = Alunos
 
     def Set_professor(self, Professor):
@@ -22,14 +26,27 @@ class Disciplina:
     def Set_id(self, ID):
         self.__ID = ID
 
-    
-    def guarda_dados(self):
+    #metodos que retornam os atributos do objeto
+    def Get_id(self):
+        return self.__ID
+
+    def Get_nome(self):
+        return self.__Nome
+
+    def Get_professor(self):
+        return self.__Professor
+
+    def Get_aluno(self):
+        return self.__Alunos
+
+    # Metodos de manipulação de arquivos
+    def Salvar_dados(self):
         arq = open('Dados_salvos/Disciplinas/'+str(self.__ID)+'.txt', 'w+')
 
         DadosParaSalvar = [self.__ID, self.__Nome, self.__Professor[0], self.__Professor[1]]
 
-        for posisao in DadosParaSalvar:
-            arq.write(str(posisao))
+        for dado in DadosParaSalvar:
+            arq.write(str(dado))
             arq.write('\t')
 
         for chave, nota in self.__Alunos.items():
@@ -41,6 +58,27 @@ class Disciplina:
             arq.write(str(nota))
         arq.close()
 
+    def Carregar_dados(self, ID):
+        dados = Carregar_arquivo(pathDisciplinas, ID, 'r+')
+        listaDadosDisciplina = dados.split('\n')
+
+        dadosgerais = listaDadosDisciplina[0].split('\t')
+        dadosgerais.pop()
+
+        dadosalunos = listaDadosDisciplina[1::]
+        dicAlunos = {}
+        for aluno in dadosalunos:
+            listaluno = aluno.split('\t')
+            dicAlunos.update({(int(listaluno[0]), listaluno[1]): int(listaluno[2])})
+
+        #print(listaDadosDisciplina, type(listaDadosDisciplina))
+        #print(dadosgerais)
+        #print(dicAlunos)
+        self.Set_id(dadosgerais[0])
+        self.Set_nome(dadosgerais[1])
+        self.Set_professor((dadosgerais[2], dadosgerais[3]))
+        self.Set_alunos(dicAlunos)
+
 
     def PrintObjeto(self):
         print(self.__ID)
@@ -48,15 +86,18 @@ class Disciplina:
         print(self.__Professor)
         print(self.__Alunos)
 
-dicAlunos = {(20210073124, 'Isau'): 8, (20211239847, 'Wagner'): 8, (20213218974, 'Tateu'): 8}
-Prof = (2022349586, 'Yuri')
+#dicAlunos = {(20210073124, 'Isau'): 8, (20211239847, 'Wagner'): 8, (20213218974, 'Tateu'): 8}
+#Prof = (2022349586, 'Yuri')
 
-poo = Disciplina()
-poo.Set_id('DCO5362')
-poo.Set_nome('Programacao orientada a objetos')
-poo.Set_aluno(dicAlunos)
-poo.Set_professor(Prof)
-poo.PrintObjeto()
+#poo = Disciplina()
+#poo.Set_id('DCO5362')
+#poo.Set_nome('Programacao orientada a objetos')
+#poo.Set_alunos(dicAlunos)
+#poo.Set_professor(Prof)
+#poo.PrintObjeto()
 
-poo.guarda_dados()
+#poo.Salvar_dados()
+
+#poo.Carregar_dados('DCO5362')
+#poo.PrintObjeto()
     
