d99459b24635a64b8dca2e4c64a3df20129f4bdd
Isau Junior <isaujr.hiago@gmail.com>
2022-06-21 00:01:20 -0300

    conseto de bug na classe disciplina

--- disciplina.py
+++ disciplina.py
@@ -2,29 +2,34 @@
 Implementação da classe Disciplina. A classe disciplina é a classe que quada os dados das displinas ministradas
 pelos professores e cursada pelos alunos. no projeto MiniSIGAA
 """
+from mailbox import NotEmptyError
 from manip_arq import Carregar_arquivo
 
 pathDisciplinas = 'dados/disciplinas/'
 
 class Disciplina:
-    def __init__(self, ID='', Nome='',  Professor = '', Alunos = ''):
+    def __init__(self, ID='', Nome=''):
         self.__ID = ID
         self.__Nome = Nome
-        self.__Professor = Professor
-        self.__Alunos = Alunos
+        self.__Professor = {}
+        self.__Alunos = {}
 
     #metodos para receber os atribudos do objeto
-    def Set_alunos(self, Alunos):
-        self.__Alunos = Alunos
+    def Set_alunos(self, ID = '', nome = '', nota =''):
+        self.__Alunos.update({ID: [nome, nota]})
 
-    def Set_professor(self, Professor):
-        self.__Professor = Professor
+    def Set_professor(self, ID = '', nome = ''):
+        self.__Professor = (ID, nome)
 
     def Set_nome(self, Nome):
         self.__Nome = Nome
 
     def Set_id(self, ID):
         self.__ID = ID
+
+    def Altera_nota(self, ID, nota):
+        nome = self.__Alunos[ID]
+        self.__Alunos[ID] = [nome[0], nota]
 
     #metodos que retornam os atributos do objeto
     def Get_id(self):
@@ -42,41 +47,41 @@ class Disciplina:
     # Metodos de manipulação de arquivos
     def Salvar_dados(self):
         with open(pathDisciplinas+str(self.__ID)+'.txt', 'w+') as arq:
-
             DadosParaSalvar = [self.__ID, self.__Nome, self.__Professor[0], self.__Professor[1]]
 
             for dado in DadosParaSalvar:
                 arq.write(str(dado))
-                arq.write('\t')
+                arq.write(':')
 
-            for chave, nota in self.__Alunos.items():
+            for chave, nome_nota in self.__Alunos.items():
                 arq.write('\n')
-                arq.write(str(chave[0]))
-                arq.write('\t')
-                arq.write(chave[1])
-                arq.write('\t')
-                arq.write(str(nota))
+                arq.write(str(chave))
+                arq.write(':')
+                arq.write(nome_nota[0])
+                arq.write(':')
+                arq.write(str(nome_nota[1]))
 
     def Carregar_dados(self, ID):
         dados = Carregar_arquivo(pathDisciplinas, ID, 'r+')
         listaDadosDisciplina = dados.split('\n')
 
-        dadosgerais = listaDadosDisciplina[0].split('\t')
+        dadosgerais = listaDadosDisciplina[0].split(':')
         dadosgerais.pop()
 
         dadosalunos = listaDadosDisciplina[1::]
         dicAlunos = {}
         for aluno in dadosalunos:
-            listaluno = aluno.split('\t')
-            dicAlunos.update({(int(listaluno[0]), listaluno[1]): int(listaluno[2])})
+            listaluno = aluno.split(':')
+            self.Set_alunos(listaluno[0], listaluno[1], listaluno[2])
+
+        del self.__Alunos['']
 
         #print(listaDadosDisciplina, type(listaDadosDisciplina))
         #print(dadosgerais)
         #print(dicAlunos)
         self.Set_id(dadosgerais[0])
         self.Set_nome(dadosgerais[1])
-        self.Set_professor((dadosgerais[2], dadosgerais[3]))
-        self.Set_alunos(dicAlunos)
+        self.Set_professor(dadosgerais[2], dadosgerais[3])
 
 
     def PrintObjeto(self):
@@ -85,17 +90,19 @@ class Disciplina:
         print(self.__Professor)
         print(self.__Alunos)
 
-dicAlunos = {(20210073124, 'Isau'): 8, (20211239847, 'Wagner'): 8}
-Prof = (2022349586, 'Yuri')
+
+
 
 poo = Disciplina()
-poo.Set_id('DCO1030')
-poo.Set_nome('Variavel aleatoria')
-poo.Set_alunos(dicAlunos)
-poo.Set_professor(Prof)
-#poo.PrintObjeto()
+poo.Set_id('DCO1032')
+poo.Set_nome('POO')
+poo.Set_alunos('', '', '')
+poo.Set_alunos('123456789', 'Isau', '8')
+poo.Set_professor('', '')
+poo.PrintObjeto()
 
 poo.Salvar_dados()
 
-#poo.Carregar_dados('DCO5362')
-#poo.PrintObjeto()
+poo.Carregar_dados('DCO1032')
+poo.Altera_nota('123456789', '7')
+poo.PrintObjeto()
